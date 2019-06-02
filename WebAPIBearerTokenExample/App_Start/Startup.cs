fb1e7a162a4dde36b140323b4566d02f9ee271ad
Vibs <vibs2006@gmail.com>
2019-06-02 14:46:52 +0530

    Initial Setup Completed for TOken Grant

---
+++ WebAPIBearerTokenExample/App_Start/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using WebAPIBearerTokenExample;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebAPIBearerTokenExample
+{
+    public class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            OAuthOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new OAuthAuthorizationServerProvider()
+                {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                    OnValidateClientAuthentication = async (context) =>
+                    {
+                        context.Validated();
+                    },
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                    OnGrantResourceOwnerCredentials = async (context) =>
+                    {
+                        if (context.UserName == "test.test@mail.com" && context.Password == "test123")
+                        {
+                            ClaimsIdentity oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            context.Validated(oAuthIdentity);
+                        }
+                    }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+                },
+                AllowInsecureHttp = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
---
+++ WebAPIBearerTokenExample/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIBearerTokenExample.Controllers
+{
+    [RoutePrefix("v1")]
+    public class DefaultController : ApiController
+    {
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Default/Ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok(new {
+                Status = "Success",
+                Descrition = "Anonymous Access tested Succesfully"
+            });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Default/TestAuthorization")]
+        public IHttpActionResult TestAuthorization()
+        {
+            return Ok(new {
+                Status = "Success",
+                Description = "Authorization Test Successful"
+            });
+        }
+
+    }
+}
