2c3d6e4ee3fa303ee0d860748cc2e227f03bb908
Vibs <vibs2006@gmail.com>
2019-06-03 22:07:03 +0530

    Completed Sample

--- WebAPIBearerTokenExample/App_Start/Startup.cs
+++ WebAPIBearerTokenExample/App_Start/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using WebAPIBearerTokenExample;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
 
 [assembly: OwinStartup(typeof(Startup))]
 
@@ -31,9 +34,51 @@ namespace WebAPIBearerTokenExample
                     {
                         if (context.UserName == "test.test@mail.com" && context.Password == "test123")
                         {
+
+                           // var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            //identity.AddClaim(new Claim("sub", context.UserName));
+                            //identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+
+                            //context.Validated(identity);
+
                             ClaimsIdentity oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            oAuthIdentity.AddClaim(new Claim("sub", context.UserName));
+                            oAuthIdentity.AddClaim(new Claim(ClaimTypes.Email, context.UserName));
                             context.Validated(oAuthIdentity);
                         }
+                        else if (context.UserName == "test.test2@mail.com" && context.Password == "test123")
+                        {
+                            ClaimsIdentity oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            oAuthIdentity.AddClaim(new Claim("sub", context.UserName));
+                            oAuthIdentity.AddClaim(new Claim(ClaimTypes.Email, context.UserName));
+                            context.Validated(oAuthIdentity);
+                        }
+                        else
+                        {
+                            context.SetError("Invalid Username / Password", "Invalid Username / Password");
+                            context.Rejected();
+                            
+                        }
+
+
+                        //Stream s = null;
+                        //context.Response.Body.CopyTo(s);
+                        //string body = new StreamReader(s).ReadToEnd();
+                        //byte[] data = Convert.FromBase64String(body);
+                        //string decodedString = Encoding.UTF8.GetString(data);
+//                        using(var bufferStream = new MemoryStream())
+//                        {
+//                            var orgBodyStream = context.Response.Body;
+//                            context.Response.Body = bufferStream;
+
+//                            bufferStream.Seek(0, SeekOrigin.Begin);
+//#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//                            bufferStream.CopyToAsync(orgBodyStream);
+//#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//                            context.Response.Body = orgBodyStream;
+//                            string s1 = new StreamReader(orgBodyStream).ReadToEnd();
+//                            Trace.WriteLine(s1);
+//                        }
                     }
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
                 },
--- WebAPIBearerTokenExample/Controllers/DefaultController.cs
+++ WebAPIBearerTokenExample/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace WebAPIBearerTokenExample.Controllers
@@ -27,10 +28,23 @@ namespace WebAPIBearerTokenExample.Controllers
         [Route("Default/TestAuthorization")]
         public IHttpActionResult TestAuthorization()
         {
+
             return Ok(new {
                 Status = "Success",
                 Description = "Authorization Test Successful"
             });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Default/TestClaims")]
+        public IHttpActionResult TestClaims()
+        {
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+
+            var claims = claimsIdentity.Claims.Select(x => new { type = x.Type, value = x.Value });
+
+            return Ok(claims);
         }
 
     }
